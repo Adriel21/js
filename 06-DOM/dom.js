5fe2cbe16f2f73ed0ee3017a04ec33d261082dc8
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-04-26 10:15:57 -0300

    Exercícios de DOM

--- 06-DOM/dom.js
+++ 06-DOM/dom.js
@@ -32,3 +32,39 @@ legenda.innerHTML = "<b>Ilustração do DOM</b>";
 // CSS via JS
 titulo.style.textAlign = "center";
 titulo.style.backgroundImage = 'linear-gradient(lightyellow, lightblue)';
+
+
+const listaEditores = document.querySelector('#lista-editores');
+
+const ultimo = listaEditores.querySelector('li:last-child');
+// ultimo.style.background = 'red';
+// ultimo.innerHTML = "<strong>Deu ruim</strong>";
+
+// const primeiro = listaEditores.querySelector('li:first-child');
+// 
+// const outro = listaEditores.querySelector('li:nth-child(2)');
+// outro.innerHTML = '<strong>Deu ruim</strong>';
+// outro.style.color = 'Red';
+
+ultimo.classList.add('destaque-item');
+
+
+//Adicionando o atributo target em todos os links da lista de referências
+//Selecionando todos os linls contidos na lista
+
+//Toda vez que usamos querySelectorALL, ele devolve uma matriz
+const links = document.querySelectorAll('ul li a');
+// links[0].style.color = 'Red';
+console.log(links);
+
+
+
+for (let i = 0; i < links.length; i++) {
+  links[i].setAttribute('target', '_blank');
+}
+
+
+
+
+
+
