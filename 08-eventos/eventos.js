9b0c1e221f463acf908f8fc9c212b377a3c24dc0
Adriel Lira <adrieldiasdossantoslira@hotmail.com>
2022-04-26 12:05:01 -0300

    eventos

---
+++ 08-eventos/eventos.js
@@ -0,0 +1,11 @@
+let nome = document.getElementById('nome').value;
+let resultado = document.getElementByid('resultado');
+
+function clique() {
+    // console.log(nome);
+    resultado.textContent = nome;
+};
+
+//Desafio - criar conversor de moedas de dolar para real - desafio para quinta feira
+
+
--- funcoes.js
+++ funcoes.js
@@ -20,8 +20,51 @@ exemplo2(); // Chamando a função
 /*Sintaxe 3 (moderna)
 Função Seta/Flecha (Arrow Function)*/
 
-var funcao = () => {   
+var exemplo3 = () => {   
     console.log("Função Seta/Flecha (Arrow Function)");
+};
+
+exemplo3();  //Chamando a função
+
+let exemplo3b = () => console.log('Arrow Function B');
+exemplo3b();
+
+//Sintaxe nomeada
+function dobra(valor) {
+   return valor*2;
+};
+
+console.log(dobra(10));
+
+//Sintaxe com Arrow Function
+
+let dobraB = (valor) => {
+    return valor*2;
+};
+console.log(dobraB(100));
+
+let dobraC = valor => valor*100;
+console.log(dobraC(20));
+
+
+
+/*Exemplos anteriores*/
+let preco = 1250;
+let desconto = 77.88;
+let precoFinal = preco - desconto;
+
+//Sintaxe padrão
+function formataMoeda(valor) {
+return valor.toLocaleString('en',{
+    style: 'currency',
+    currency: 'USD'});
 }
 
-funcao();  //Chamando a função
+//Sintaxe arrow
+let formataValor = valor => {
+return valor.toLocaleString('JP', {
+    style: 'currency',
+    currency: 'BRL'});
+}
+
+console.log(formataValor(7777))
